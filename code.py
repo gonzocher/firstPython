b5ea57c414e4c2d78a8ef35518aa1423a0a20c75
Cheri Haggard <cheri@MATHmaniaRobotics.com>
2018-12-01 15:46:35 -0800

    Create python file

---
+++ code.py
@@ -0,0 +1,14 @@
+name = input ("What is your name? ")
+print("Hi "+name+ ".")
+print ("")
+print ("Which adventure are you up for today? (Type the number)")
+option = input ("1. Soccer or 2. Camping or 3. Dancing:   ")
+
+if option == "1":
+  print("Soccer adventure")
+elif option == "2":
+  print("Camping adventure")
+elif option == "3":
+  print("Dancing adventure  ")
+else:
+  print("Please type 1 or 2 or 3.")
